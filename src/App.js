1301cfee84c4e3339c0626b386b5fa7d51548293
tejas-muthya37 <55662912+tejas-muthya37@users.noreply.github.com>
2022-05-10 18:03:08 +0530

    Feat: Initial setup for Pomodoro

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <textarea></textarea>
+    </div>
+  );
 }
 
 export default App;
